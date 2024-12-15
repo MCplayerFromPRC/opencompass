61d58ff090785aacb854d86cffc25d5544d1f497
changcheng <changcheng@pjlab.org.cn>
2024-12-15 16:34:01 +0800

    ruler base model removing

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from opencompass.cli.main import main
+import sys
+from pathlib import Path
+
+sys.path.insert(0, Path(__file__).parent.as_posix())
+
+from opencompass.cli.main import main  # noqa: E402 # pylint: disable=C0413
 
 if __name__ == '__main__':
     main()
